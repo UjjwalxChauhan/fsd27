98c3051ba214c5625f9440fc947c5fd3201ba867
UjjwalxChauhan <ujjwalc081@gmail.com>
2026-02-25 16:24:46 +0530

    Added js folder

---
+++ JS/15Ques.js
@@ -0,0 +1,20 @@
+
+// longest shared prefix
+
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === "") return "";
+        }
+    }
+
+    return prefix;
+}
+
+let s = ["flower","floral","float","flow"];
+console.log(longestCommonPrefix(s));  // "flo"
